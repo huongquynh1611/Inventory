44b3cac01f2e6a3ae7a9736b562aad597de56a46
huongquynh1611 <75553332+huongquynh1611@users.noreply.github.com>
2021-03-08 17:31:58 +0700

    change link

--- aliproductlist.py
+++ aliproductlist.py
@@ -50,117 +50,125 @@ class DataProductList:
     def __init__(self, products, subcategories):
         self.Products = products
         self.SubCategories = subcategories
-def GetProductList(max_num_pages,url):
+def GetProductList(url):
     list_product = []   
     url =  url
-    for ele in range(1,max_num_pages+1):              
-        driver = webdriver.Chrome(executable_path = "C:\\Users\\tlhqu\\Downloads\\chromedriver_win32\\chromedriver.exe", chrome_options=chrome_options)   
-        print(" >> ",url + str(ele) + "&isrefine=y" )
-        driver.get(url + str(ele) + "&isrefine=y" )
-        try:
-            WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME , 'product-container')))
-            print("Page is ready!")
-        except TimeoutException:
-            print ("Loading took too much")
-            continue
-        scrolls = 6
-        while True:
-            scrolls -= 1
-            time.sleep(1)
-            driver.execute_script("window.scrollBy(0, 1080)")
-            if scrolls < 0:
-                break
-        # Lấy html từ page source 
-        _page = BeautifulSoup(driver.page_source, "html.parser")
-        driver.quit()
-        scate_list = []
-        scate_name_field = "Subcate_name"
-        scate_link_field = "Subcate_link"
-        sub = _page.findAll("ul",{"class":"child-menu"})
-        sub = sub[0].findAll("li") if len(sub) > 0 else ""
-        for i in sub: 
-            scate_id = i['ae_object_value']
-            print(scate_id)
-            scate_name = i.a.text.strip()
-            scate_url = i.a["href"]
-            scate_str = SubCategory(scate_name,scate_url,scate_id)
-            scate_list.append(scate_str)
+                  
+    driver = webdriver.Chrome(executable_path = "C:\\Users\\tlhqu\\Downloads\\chromedriver_win32\\chromedriver.exe", chrome_options=chrome_options)   
+    print(" >> ",url  )
+    driver.get(url  )
+    try:
+        WebDriverWait(driver, 1).until(EC.presence_of_element_located((By.CLASS_NAME , 'product-container')))
+        print("Page is ready!")
+    except TimeoutException:
+        print ("Loading took too much")
 
-        post=_page.findAll("li",{"class":"list-item"})
+    scrolls = 6
+    while True:
+        scrolls -= 1
+        time.sleep(1)
+        driver.execute_script("window.scrollBy(0, 1080)")
+        if scrolls < 0:
+            break
+    # Lấy html từ page source 
+    _page = BeautifulSoup(driver.page_source, "html.parser")
+    driver.quit()
+    scate_list = []
+    scate_name_field = "Subcate_name"
+    scate_link_field = "Subcate_link"
+    sub = _page.findAll("ul",{"class":"child-menu"})
+    sub = sub[0].findAll("li") if len(sub) > 0 else ""
+    for i in sub: 
+        scate_id = i['ae_object_value']
+        print(scate_id)
+        scate_name = i.a.text.strip()
+        scate_url = i.a["href"]
+        scate_str = SubCategory(scate_name,scate_url,scate_id)
+        scate_list.append(scate_str)
+
+    post=_page.findAll("li",{"class":"list-item"})
+
+    list_product_str = []
     
-        list_product_str = []
-       
-        for i in post:
-            
-            product_url = i.a["href"]
-            product_title = i.text.strip()
-            product_price_current = i.findAll("span",{"class":"price-current"})
-            product_price_current = product_price_current[0].text.strip() if len(product_price_current)>0 else ""
-            product_price_origin =  i.findAll("span",{"class":"price-original"})
-            product_price_origin = product_price_origin[0].text.strip() if len(product_price_origin)>0 else ""         
-            product_price_discount = i.findAll("span",{"class":"price-discount"})
-            product_price_discount = product_price_discount[0].text.strip() if len( product_price_discount)>0 else ""
-            product_shipping = i.findAll("span",{"class":"shipping-value"})
-            product_shipping = product_shipping[0].text.strip() if len( product_shipping)>0 else ""
-            product_rate = i.findAll("span",{"class":"rating-value"})
-            product_rate = product_rate[0].text.strip() if len(product_rate)>0 else ""
-            product_store = i.findAll("div",{"class":"item-store-wrap"})
-            product_store = product_store[0].a.text.strip() if len(product_store)>0 else ""
-            product_store_link = i.findAll("div",{"class":"item-store-wrap"})
-            product_store_link = product_store_link[0].a['href'] if len(product_store_link)>0 else ""
-            product_img_link = i.findAll("div",{"class":"product-img"})[0].a.img["src"].replace(".jpg_220x220xz","")
-            product_img_name = i.findAll("div",{"class":"product-img"})[0].a.img["alt"]
-            
-            product_str = Product(product_title,product_url,product_price_current,product_price_origin,product_price_discount,product_shipping,product_rate,product_store,product_store_link,product_img_link)
-            list_product_str.append(product_str)
+    for i in post:
         
-           
-        data = DataProductList(list_product_str,scate_list)
+        product_url = i.a["href"]
+        product_title = i.text.strip()
+        product_price_current = i.findAll("span",{"class":"price-current"})
+        product_price_current = product_price_current[0].text.strip() if len(product_price_current)>0 else ""
+        product_price_origin =  i.findAll("span",{"class":"price-original"})
+        product_price_origin = product_price_origin[0].text.strip() if len(product_price_origin)>0 else ""         
+        product_price_discount = i.findAll("span",{"class":"price-discount"})
+        product_price_discount = product_price_discount[0].text.strip() if len( product_price_discount)>0 else ""
+        product_shipping = i.findAll("span",{"class":"shipping-value"})
+        product_shipping = product_shipping[0].text.strip() if len( product_shipping)>0 else ""
+        product_rate = i.findAll("span",{"class":"rating-value"})
+        product_rate = product_rate[0].text.strip() if len(product_rate)>0 else ""
+        product_store = i.findAll("div",{"class":"item-store-wrap"})
+        product_store = product_store[0].a.text.strip() if len(product_store)>0 else ""
+        product_store_link = i.findAll("div",{"class":"item-store-wrap"})
+        product_store_link = product_store_link[0].a['href'] if len(product_store_link)>0 else ""
+        product_img_link = i.findAll("div",{"class":"product-img"})[0].a.img["src"].replace(".jpg_220x220xz","")
+        product_img_name = i.findAll("div",{"class":"product-img"})[0].a.img["alt"]
+        
+        product_str = Product(product_title,product_url,product_price_current,product_price_origin,product_price_discount,product_shipping,product_rate,product_store,product_store_link,product_img_link)
+        list_product_str.append(product_str)
+    
+        
+    data = DataProductList(list_product_str,scate_list)
 
- 
-        query = [
-            {
-                "Products": [
-                    [       
-                        "Name",
-                        "URL",
-                        "PriceCurrent",
-                        "PriceOriginal" ,
-                        "PriceDiscount",
-                        "Shipping",
-                        "Rate",
-                        "StoreName",
-                        "StoreLink",
-                        "ImgLink"
-                    ]
+
+    query = [
+        {
+            "Products": [
+                [       
+                    "Name",
+                    "URL",
+                    "PriceCurrent",
+                    "PriceOriginal" ,
+                    "PriceDiscount",
+                    "Shipping",
+                    "Rate",
+                    "StoreName",
+                    "StoreLink",
+                    "ImgLink"
                 ]
-            },
-            {
-                "SubCategories": [
-                    [
-                        "SubCateName",
-                        "SubCateURL",
-                        "SubCateID"
-                    ]
+            ]
+        },
+        {
+            "SubCategories": [
+                [
+                    "SubCateName",
+                    "SubCateURL",
+                    "SubCateID"
                 ]
-            }
-        ]
+            ]
+        }
+    ]
 
-        std_info = dictfier.dictfy(data, query)
+    std_info = dictfier.dictfy(data, query)
 
-        file_name = uuid.uuid4().hex + ".json"
+    file_name = uuid.uuid4().hex + ".json"
 
-        completeName = os.path.join(os.getcwd(), file_name)
+    completeName = os.path.join(os.getcwd(), file_name)
 
-        file = open(completeName, "w", encoding="utf8")
-        file.write(json.dumps(std_info))
-        file.close()
+    file = open(completeName, "w", encoding="utf8")
+    file.write(json.dumps(std_info))
+    file.close()
 
-        print(completeName)
-
-
-url = "https://vi.aliexpress.com/af/category/200003455.html?trafficChannel=af&catName=parkas&CatId=200003455&ltype=affiliate&SortType=default&page="
-GetProductList(max_num_pages=2,url=url)               
+    print(completeName)
+cateid=200000662
+catename = 'men-clothing'
+sortType = ['create_desc','total_tranpro_desc','default','price_asc']
+sortType = "default"
+shipfrom = ['CN','VN']
+shipfrom = ''
+minPrice = ''
+maxPrice = ''
+text = ''
+page = 1
+url = "https://vi.aliexpress.com/category/" + str(cateid) + "/"+ catename +".html?trafficChannel=main&ltype=wholesale&SortType="+ sortType + "&SearchText="+ text + "&shipFromCountry="+ shipfrom + "&minPrice=" +minPrice +"&maxPrice=" + maxPrice+ "&SortType=" + sortType +"&page=" + str(page)
+GetProductList(url=url)               
            
 
 
